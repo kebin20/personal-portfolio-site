8319a7e3192f08141787b553158e3c8f5dd9ee64
kebin20 <ktanzyl@hotmail.com>
2023-04-14 22:25:05 +0900

    feat: added skills showcase

---
+++ src/components/SkillsShowcase.js
@@ -0,0 +1,46 @@
+function SkillsShowcase() {
+  return (
+    <section class="skills-showcase">
+      <ul class="skills-showcase__list">
+        <li class="skills-showcase__list-item">
+          <i class="devicon-html5-plain"></i>
+        </li>
+        <li class="skills-showcase__list-item">
+          <i class="devicon-css3-plain"></i>
+        </li>
+        <li class="skills-showcase__list-item">
+          <i class="devicon-javascript-plain"></i>
+        </li>
+        <li class="skills-showcase__list-item">
+          <i class="devicon-react-original"></i>
+        </li>
+        <li class="skills-showcase__list-item">
+          <i class="devicon-typescript-original"></i>
+        </li>
+        <li class="skills-showcase__list-item">
+          <i class="devicon-git-plain"></i>
+        </li>
+        <li class="skills-showcase__list-item">
+          <i class="devicon-vscode-plain"></i>
+        </li>
+        <li class="skills-showcase__list-item">
+          <i class="devicon-tailwindcss-plain"></i>
+        </li>
+        <li class="skills-showcase__list-item">
+          <i class="devicon-github-plain"></i>
+        </li>
+        <li class="skills-showcase__list-item">
+          <i class="devicon-sass-plain"></i>
+        </li>
+        <li class="skills-showcase__list-item">
+          <i class="devicon-firebase-plain"></i>
+        </li>
+        <li class="skills-showcase__list-item">
+          <i class="devicon-figma-plain"></i>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default SkillsShowcase;
