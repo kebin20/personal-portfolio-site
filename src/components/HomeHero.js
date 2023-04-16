297ebb61b5bb2148a2d11c0aaa05788be8d5ceb1
kebin20 <ktanzyl@hotmail.com>
2023-04-16 14:40:53 +0900

    chore: reorganised layout for hero content

--- src/components/HomeHero.js
+++ src/components/HomeHero.js
@@ -1,17 +1,19 @@
 import Button from "./UI/Button";
+import Image from "next/image";
+import avatar from "../assets/kevin-avatar.png";
 
 function HomeHero() {
   return (
     <section id="home-hero" className="home-hero">
       <div className="home-hero__content">
+        <Image src={avatar} alt="personal profile picture" className="home-hero__content__hero-picture"/>
         <h1 className="home-hero__content__heading">
-          Hey there, <br />
-          I&apos;m Kevin Tanzyl !
+          Hey there, I&apos;m Kevin Tanzyl !
         </h1>
-        <p className="home-hero__content__info">
+        <h2 className="home-hero__content__sub-heading">
           A Web Developer Building Awesome Webapps And Websites That Powers The
           Internet
-        </p>
+        </h2>
         <div className="btn-container">
           <Button className="btn">Projects</Button>
           <Button className="btn btn-alternate">Contact</Button>
