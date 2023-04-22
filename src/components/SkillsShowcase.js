3d4cc14e1b3a24b6de4a3622c36e5f5da8deb4ee
kebin20 <ktanzyl@hotmail.com>
2023-04-22 12:39:10 +0900

    style: added div to hold skills showcase list for accessiblity reasons

--- src/components/SkillsShowcase.js
+++ src/components/SkillsShowcase.js
@@ -1,44 +1,40 @@
 function SkillsShowcase() {
   return (
     <section className="skills-showcase">
-      <ul className="skills-showcase__list">
-        <li className="skills-showcase__list-item">
-          <i className="devicon-html5-plain"></i>
-        </li>
-        <li className="skills-showcase__list-item">
-          <i className="devicon-css3-plain"></i>
-        </li>
-        <li className="skills-showcase__list-item">
-          <i className="devicon-javascript-plain"></i>
-        </li>
-        <li className="skills-showcase__list-item">
-          <i className="devicon-react-original"></i>
-        </li>
-        <li className="skills-showcase__list-item">
-          <i className="devicon-typescript-original"></i>
-        </li>
-        <li className="skills-showcase__list-item">
-          <i className="devicon-git-plain"></i>
-        </li>
-        <li className="skills-showcase__list-item">
-          <i className="devicon-vscode-plain"></i>
-        </li>
-        <li className="skills-showcase__list-item">
-          <i className="devicon-tailwindcss-plain"></i>
-        </li>
-        {/* <li className="skills-showcase__list-item">
-          <i className="devicon-github-plain"></i>
-        </li> */}
-        <li className="skills-showcase__list-item">
-          <i className="devicon-sass-plain"></i>
-        </li>
-        <li className="skills-showcase__list-item">
-          <i className="devicon-firebase-plain"></i>
-        </li>
-        {/* <li className="skills-showcase__list-item">
-          <i className="devicon-figma-plain"></i>
-        </li> */}
-      </ul>
+      <div>
+        <ul className="skills-showcase__list">
+          <li className="skills-showcase__list-item">
+            <i className="devicon-html5-plain"></i>
+          </li>
+          <li className="skills-showcase__list-item">
+            <i className="devicon-css3-plain"></i>
+          </li>
+          <li className="skills-showcase__list-item">
+            <i className="devicon-javascript-plain"></i>
+          </li>
+          <li className="skills-showcase__list-item">
+            <i className="devicon-react-original"></i>
+          </li>
+          <li className="skills-showcase__list-item">
+            <i className="devicon-typescript-original"></i>
+          </li>
+          <li className="skills-showcase__list-item">
+            <i className="devicon-git-plain"></i>
+          </li>
+          <li className="skills-showcase__list-item">
+            <i className="devicon-vscode-plain"></i>
+          </li>
+          <li className="skills-showcase__list-item">
+            <i className="devicon-tailwindcss-plain"></i>
+          </li>
+          <li className="skills-showcase__list-item">
+            <i className="devicon-sass-plain"></i>
+          </li>
+          <li className="skills-showcase__list-item">
+            <i className="devicon-firebase-plain"></i>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 }
