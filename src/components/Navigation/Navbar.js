4f0c42ade521a9d80a07e5417b9396bb7e10404c
kebin20 <ktanzyl@hotmail.com>
2023-05-11 19:03:01 +0900

    feat: added script for navbar to change color when scrolled

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import logo from "../../assets/kevin-avatar-light.svg";
 
@@ -7,15 +7,33 @@ import NavItem from "./NavItem";
 
 export default function NavBar() {
   const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  const navItems = ["home", "about", "projects", "contact"];
 
   function toggleHamburgerMenu() {
     setToggle((prevToggle) => !prevToggle);
   }
 
-  const navItems = ["home", "about", "projects", "contact"];
+  function handleScroll() {
+    const offset = window.scrollY;
+    if (offset > 50) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <header className="header">
+    <header className={`header ${scrolled ? "scrolled" : ""}`}>
       <nav className="nav">
         <Image className="logo" alt="personal logo" src={logo} />
         <h4 className="logo-title">Kevin Tanzyl</h4>
