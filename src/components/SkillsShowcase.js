81644bbc35c8b89d683fda4ea2538304295074c5
kebin20 <ktanzyl@hotmail.com>
2023-05-11 18:39:08 +0900

    style: added styled-icon and adjusted layout for skill icons

--- src/components/SkillsShowcase.js
+++ src/components/SkillsShowcase.js
@@ -1,3 +1,5 @@
+import { Styledcomponents } from "@styled-icons/simple-icons/Styledcomponents";
+
 function SkillsShowcase() {
   return (
     <section className="skills-showcase">
@@ -28,10 +30,16 @@ function SkillsShowcase() {
             <i className="devicon-tailwindcss-plain"></i>
           </li>
           <li className="skills-showcase__list-item">
+            <Styledcomponents style={{ width: "6rem" }} />
+          </li>
+          <li className="skills-showcase__list-item">
             <i className="devicon-sass-plain"></i>
           </li>
           <li className="skills-showcase__list-item">
             <i className="devicon-firebase-plain"></i>
+          </li>
+          <li className="skills-showcase__list-item">
+            <i className="devicon-figma-plain"></i>
           </li>
         </ul>
       </div>
