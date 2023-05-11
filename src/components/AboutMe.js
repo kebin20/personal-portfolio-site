02be6ebf76142407cb09aac0fc7f8581819538dc
kebin20 <ktanzyl@hotmail.com>
2023-05-11 18:19:45 +0900

    feat: added a link to my blog

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,8 +4,8 @@ function AboutUs() {
       <div className="main-container">
         <h2 className="main-container__sm-title">About Me 🧑</h2>
         <h3 className="main-container__main-title">
-          Here you will find information about me and a list of
-          my current skills in programming
+          Here you will find information about me and a list of my current
+          skills in programming
         </h3>
         <div className="main-section">
           <div className="info">
@@ -13,13 +13,35 @@ function AboutUs() {
               <h3 className="info__main-info__title">Personal Summary </h3>
               <p className="info__main-info__text-primary">
                 <span>
-                I&apos;m a highly motivated junior web developer with a year of experience in creating visually stunning and functionally impressive websites. I&apos;m passionate about blending design and functionality to deliver a user-friendly experience that exceeds client expectations. I have the skills to create responsive layouts that look great on any device, and I&apos;m committed to translating your ideas into an engaging online presence. Let&apos;s work together to bring your website dreams to life.
+                  I&apos;m a highly motivated junior web developer with a year
+                  of experience in creating visually stunning and functionally
+                  impressive websites. I&apos;m passionate about blending design
+                  and functionality to deliver a user-friendly experience that
+                  exceeds client expectations. I have the skills to create
+                  responsive layouts that look great on any device, and I&apos;m
+                  committed to translating your ideas into an engaging online
+                  presence. Let&apos;s work together to bring your website
+                  dreams to life.
                 </span>
-                <span>Come and have a look at my projects below!</span>
+                <span>
+                  Come and have a look at my projects below and see my blog for
+                  my learning takeaways!
+                </span>
               </p>
               <div className="btn-container">
-                <a href="#projects" className="btn-container btn" aria-label="Go to projects">
+                <a
+                  href="#projects"
+                  className="btn-container btn"
+                  aria-label="Go to projects"
+                >
                   Projects
+                </a>
+                <a
+                  href="https://dev.to/kebin20"
+                  className="btn btn-alternate"
+                  aria-label="Go to my personal blog on dev.to"
+                >
+                  Blog
                 </a>
               </div>
             </div>
